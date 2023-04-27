586ee86f05c1e5ad8a37ebe349938903a1db9e0d
ramboo-raajesh <100186781+ramboo-raajesh@users.noreply.github.com>
2023-04-27 21:17:56 +0530

    Prompt added

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import requests
 
+#asking user to enter the location
+print("Which city you want to see the weather")
+city = input()
+
+#creating full URL with user's creditionals
 params = {
   'access_key': '02aee7fd5aed7f3acf3f6b9e15d883b0',
-  'query': 'Chennai'
+  'query': city
 }
 
 api_result = requests.get('http://api.weatherstack.com/current', params)
-
 api_response = api_result.json()
 
-print("In chennai the tempreature is: ", api_response['current']['temperature']," C",api_response['current']['weather_descriptions'],api_response['current']['humidity'],api_response['current']['feelslike'])
+#Printing the short report
+print("In {} the tempreature is: {}C".format(api_response['location']['name'],api_response['current']['temperature']))
+
+#prompting for further details
+print("Do you wanna see further details...! yes/no")
+details =  input()
+if details == "yes":
+    print(api_response)
+else:
+    pass
