f70278285cf21ad3e8954c36032ac9ceb27db607
ramboo-raajesh <100186781+ramboo-raajesh@users.noreply.github.com>
2023-04-28 20:07:19 +0530

    headingsssss
    The detailed information are now categorized

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-import os
 import requests
 
-city = input("Enter the city: ")
+#asking user to enter the location
+print("Which city you want to see the weather")
+city = input()
 
+#creating full URL with user's credentials
 params = {
   'access_key': '02aee7fd5aed7f3acf3f6b9e15d883b0',
   'query': city
 }
 
 api_result = requests.get('http://api.weatherstack.com/current', params)
+api_response = api_result.json()
+print("In {} there is {}°C\n".format(api_response['location']['name'],api_response['current']['temperature']))
 
-try:
-    # Make the API request and parse the response
-    response = api_result
-    response.raise_for_status()  # raise an exception if the response status code is not 200
-    data = response.json()
+# check if user wants to see extra details
+extra = input("Do you want to see extra details? (y/n): ")
 
-    # Extract the weather information and print it to the console
-    location = data['location']['name']
-    temperature = data['current']['temperature']
-    print(f"In {location} the temperature is: {temperature} C")
+# print categorized data based on user input
+if extra.lower() == "y":
+    print("\nWeather Data:")
+    print("Temperature:", api_response['current']['temperature'], "C")
+    print("Weather Description:", api_response['current']['weather_descriptions'][0])
+    print("Wind Speed:", api_response['current']['wind_speed'], "km/h")
+    print("Humidity:", api_response['current']['humidity'], "%")
+    print("Cloud Cover:", api_response['current']['cloudcover'], "%")
 
-except requests.exceptions.HTTPError as e:
-    print(f"HTTP error: {e}")
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
-except KeyError as e:
-    print(f"Response error: {e} key not found in response")
+    print("\nLocation Data:")
+    print("Name:", api_response['location']['name'])
+    print("Country:", api_response['location']['country'])
+    print("Region:", api_response['location']['region'])
+    print("Latitude:", api_response['location']['lat'])
+    print("Longitude:", api_response['location']['lon'])
+
+    print("\nTime Data:")
+    print("Timezone ID:", api_response['location']['timezone_id'])
+    print("Local Time:", api_response['location']['localtime'])
+    print("UTC Offset:", api_response['location']['utc_offset'])
+else:
+    print("Temperature:", api_response['current']['temperature'], "C")
+    print("Weather Description:", api_response['current']['weather_descriptions'][0])
